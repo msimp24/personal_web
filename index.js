7c2a61fc5100d6d1507490896dcce2aef6042fcd
Mark Simpson <mark.simpson4@gmail.com>
2022-10-24 15:21:07 +0300

    Updated changes to personal web

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 const express = require('express')
 const app = express();
 
+const nodemailer = require('nodemailer');
+
 app.listen(3000, ()=>{
     console.log('listening at port 3000');
 })
 
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
+
+app.post('/email', (request, response) =>{
+    console.log('I got a request'); 
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user:'mark.simpson4@gmail.com',
+            pass:'noilnodmlnovqees'
+        }
+    })
+
+    const mailOptions = {
+        from: request.body.email,
+        to: 'mark.simpson4@gmail.com',
+        subject: `Message from ${request.body.firstName} ${request.body.lastName} (${request.body.email}): ${request.body.subject} `,
+        text: request.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) =>{
+        if(error){
+            console.log(error);
+            response.send('Error')
+        }
+        else{
+            console.log('Email sent successfully')
+            response.send("success");
+        }
+    })
+
+
+    const data = request.body;
+    response.json(data);
+    
+})
--- public/app.js
+++ public/app.js
@@ -1,20 +1,75 @@
+
 const enterBtn = document.querySelector(".square");
 const topPos = enterBtn.offsetTop; 
-const navBar = document.querySelector('#fixed-nav-bar');
+const navBar = document.querySelector('.fixed-nav-bar');
+const boxes = document.querySelectorAll(".nav-item");
+var currWindowWidth = window.innerWidth;
+var currYPos = window.scrollY;
+
 
 //buttons for webpages
 
 const aboutMeBtn = document.querySelector("#about-me-btn");
-const workExpBtn = document.querySelector("#work-exp-btn");
+const skillsBtn = document.querySelector("#skills-btn");
+const contactBtn = document.querySelector("#contact-btn");
 
 //web page headers
 const aboutMePg = document.querySelector("#about-me-pg");
-const workExpPg = document.querySelector("#work-experience-pg")
+const skillsPg = document.querySelector("#skills-pg");
+const contactPg = document.querySelector("#contact-pg");
 
+//contact me text fields/buttons
+const firstNameTxt = document.querySelector('#first-name');
+const lastNameTxt = document.querySelector('#last-name');
+const emailTxt = document.querySelector('#inputEmail');
+const subjectTxt = document.querySelector('#inputSubject');
+const messageTxt = document.querySelector('#inputMessage');
+const submitBtn = document.querySelector('#submit-button');
+
+
+
+// down arrow button
 var rect = enterBtn.getBoundingClientRect();
 
 
 
+window.addEventListener('scroll', () =>{
+    currYPos = window.scrollY;
+    if(currYPos > (rect.top)){
+        if(currWindowWidth > 800){
+            navBar.style.display = 'initial';
+        }
+        else{
+            navBar.style.display = "none";
+            
+        }
+    }
+    else{
+        navBar.style.display = "none";
+    }
+})
+
+window.addEventListener('resize', () =>{
+    currWindowWidth = window.innerWidth;
+    
+    if(currWindowWidth > 800){
+        if(currYPos > rect.top){
+            navBar.style.display = 'initial';
+        }
+        else{
+            navBar.style.display = 'none'
+        }
+
+    }
+    else{
+        navBar.style.display = 'none';
+    }
+
+    console.log(currWindowWidth);
+
+})
+
+// event listeners for navbar buttons
 enterBtn.addEventListener('click', ()=>{
     scrolldiv(aboutMePg);
     navBar.style.display = "block";
@@ -24,14 +79,55 @@ aboutMeBtn.addEventListener('click', () =>{
     scrolldiv(aboutMePg);
     window.location.href ="#/about-me";
 })
-workExpBtn.addEventListener('click', () =>{
-    scrolldiv(workExpPg);
-    window.location.href ="#/work-experience";
+skillsBtn.addEventListener('click', () =>{
+    scrolldiv(skillsPg);
+    window.location.href ="#/skills";
+})
+contactBtn.addEventListener('click', () =>{
+    scrolldiv(contactPg);
+    window.location.href ="#/contact";
 })
 
-window.addEventListener('scroll', () =>{
-    console.log(navBar.getBoundingClientRect().top + window.scrollY);
+submitBtn.addEventListener('click', async ()=>{
+    var firstName = firstNameTxt.value;
+    var lastName = lastNameTxt.value;
+    var email = emailTxt.value;
+    var subject = subjectTxt.value;
+    var message = messageTxt.value;
+
+    var data = {firstName, lastName, email, subject, message};
+
+    const options = {
+        method: 'POST', 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    }
+
+
+    await fetch('/email', options);
+
+    firstNameTxt.value = "";
+    lastNameTxt.value = "";
+    emailTxt.value = "";
+    subjectTxt.value = "";
+    messageTxt.value = "";
+
 })
+
+//changes nav box background color when hovered over
+
+boxes.forEach(box =>{
+    box.addEventListener('mouseover', () =>{
+        box.classList.remove('bg-dark');
+        box.children[0].classList.remove('text-white');
+        box.children[0].classList.add('text-black');
+    })
+    box.addEventListener('mouseout', ()=>{
+         box.classList.add('bg-dark')
+         box.children[0].classList.add('text-white');
+    })
+})
+
 
 // functions for smooth scroll to specific points in the
 function scrolldiv(obj) {
@@ -47,3 +143,5 @@ function findPosition(obj) {
         return [currenttop];
     }
 }
+
+
